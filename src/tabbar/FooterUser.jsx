b67ac9bcc70d94c1c4c7d70019cae4ffa528e595
Jubujubu <p15187618@gmail.com>
2025-04-09 23:18:30 +0700

    Create FooterUser.jsx

---
+++ src/tabbar/FooterUser.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Footer.css'
+import { Link, Outlet } from 'react-router-dom'
+function Footer() {
+    return (
+        <div className='footer'>
+            <div className='footer-top'>
+                        <div></div>
+                        <Link to={'/user'}>
+                            <span className='boxlogo'>
+                                <img className='logo' src="/images/Logo.jpg" />   Diacare
+                            </span>
+                        </Link>
+            
+                            <Link to={'/user'}>หน้าหลัก</Link>
+                            <Link to={'/user/predict'}>วิเคราะความเสี่ยง</Link>
+                            <Link to={'/user/history'}>ประวัติ</Link>
+                        <div>
+                            <div className='f-footer-top'>ติดต่อสอบถาม</div>
+                            <div className='f-footer-top-2'>diacare.healthcare@gmail.com<br />
+                            (+66)00-000-0000
+                            </div>
+                        </div>
+                    </div>
+            <div className='button'>copyright © 2024 all rights reserved</div>
+        </div>
+    )
+}
+
+export default Footer
